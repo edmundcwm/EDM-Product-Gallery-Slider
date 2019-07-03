1ff96b668e64d29df48ddc28ec7a1fb9783fec45
Edmund Chan <edmund@nerb.com.sg>
2019-07-03 22:17:06 +0800

    Add $plugin_name variable to EDM_Product_Gallery_Slider class

--- includes/class-edm-product-gallery-slider-dependencies.php
+++ includes/class-edm-product-gallery-slider-dependencies.php
@@ -1,21 +1,36 @@
 <?php
 
 /**
- * Checks if WooCommerce is Enabled
+ * Checks for dependencies
  */
 
 defined( 'ABSPATH' ) || exit;
 
 class EDM_Product_Gallery_Slider_Dependencies {
 
+    /**
+     * The list of active plugins
+     * 
+     * @since  1.0.0
+     * @access private
+     * @var    array $active_plugins Stores the list of active plugins
+     */
     private $active_plugins;
-    private $plugin_name;
+    
+    /**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
 
     public function __construct( $plugin_name ) {
         $this->plugin_name = $plugin_name;
         $this->active_plugins = get_option( 'active_plugins' );
     }
-
+    
     public function wc_active_check() {
         return in_array( 'woocommerce/woocommerce.php', $this->active_plugins );
     }
