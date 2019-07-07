8ca7a330315e7ad2c20fe5cd02a737f452a66a8a
Edmund Chan <edmund@nerb.com.sg>
2019-07-08 07:38:58 +0800

    Remove excess spacings

--- includes/class-edm-product-gallery-slider.php
+++ includes/class-edm-product-gallery-slider.php
@@ -108,9 +108,6 @@ class EDM_Product_Gallery_Slider {
 		$this->filters = $this->add_hook_to_collection( $this->filters, 'woocommerce_single_product_photoswipe_enabled', $plugin_public, 'disable_photo_swipe' );
 		$this->filters = $this->add_hook_to_collection( $this->filters, 'woocommerce_single_product_image_thumbnail_html', $plugin_public, 'use_main_image', 10, 2 );
 		$this->filters = $this->add_hook_to_collection( $this->filters, 'woocommerce_single_product_carousel_options', $plugin_public, 'product_carousel_options', 10, 2 );
-
-		
-		
 	}
 
 	/**
