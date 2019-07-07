64f0a2198f8254be87bd2df07afc37f6fefec3a9
Edmund Chan <edmund@nerb.com.sg>
2019-07-07 23:30:50 +0800

    Add JS function enabling carousel on product thumbnails and use it as a navigation for product gallery

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,5 +1,23 @@
 var $ = jQuery.noConflict();
 
-$('.woocommerce-product-gallery__wrapper').slick({
-    adaptiveHeight: true,
+$('#product-gallery').flexslider({
+    animation: "slide",
+    controlNav: false,
+    directionNav: false,
+    animationLoop: false,
+    slideshow: false,
+    selector: ".woocommerce-product-gallery__wrapper > div",
+    sync: "#product-thumbnails"
+});
+
+$('#product-thumbnails').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 80,
+    itemMargin: 5,
+    maxItems: 4,
+    asNavFor: '#product-gallery',
+    selector: ".woocommerce-product-gallery-thumbnails__wrapper > div",
 });
