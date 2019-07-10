3da4e35001c023da50fbc0fc418efaa72f9d32a4
Edmund Chan <edmund@nerb.com.sg>
2019-07-10 22:25:43 +0800

    Add new file to contain all template related functions

---
+++ public/edm-product-gallery-template-functions.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Override WC woocommerce_show_product_images()
+ */
+if ( ! function_exists( 'woocommerce_show_product_images' ) ) {
+
+	/**
+	 * Output the product image before the single product summary.
+	 */
+	function woocommerce_show_product_images() {
+		wc_get_template( 'single-product/product-image.php' );
+	}
+}
+
+/**
+ * Get HTML for a gallery thumbnail.
+ *
+ * Woocommerce_gallery_thumbnail_size, woocommerce_gallery_image_size and woocommerce_gallery_full_size accept name based image sizes, or an array of width/height values.
+ *
+ * @since 1.0.0
+ * @param int  $attachment_id Attachment ID.
+ * @return string
+ */
+function edm_get_gallery_thumbnail( $attachment_id ) {
+	$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
+	$thumbnail_size    = apply_filters( 'edm_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
+	$full_size         = apply_filters( 'edm_gallery_full_size', apply_filters( 'edm_product_thumbnails_large_size', 'full' ) );
+	$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, $thumbnail_size );
+	$alt_text          = trim( wp_strip_all_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) );
+	$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
+	$image             = wp_get_attachment_image( $attachment_id, $thumbnail_size );
+
+	return '<div data-thumb="' . esc_url( $thumbnail_src[0] ) . '" data-thumb-alt="' . esc_attr( $alt_text ) . '" class="woocommerce-product-gallery__image"><a href="' . esc_url( $full_src[0] ) . '">' . $image . '</a></div>';
+}
