1714e1ec3741066c7ce85fea7013ccbf0828ea30
Edmund Chan <edmund@nerb.com.sg>
2019-07-18 18:24:41 +0800

    Remove spacings

--- edm-product-gallery-slider.php
+++ edm-product-gallery-slider.php
@@ -15,10 +15,8 @@ defined( 'ABSPATH' ) || exit;
 require_once plugin_dir_path( __FILE__ ) . '/includes/class-edm-product-gallery-slider.php';
 
 function edm_product_gallery_slider_run() {
-
 	$instance = new EDM_Product_Gallery_Slider();
 	$instance->run();
-
 }
 
 edm_product_gallery_slider_run();
