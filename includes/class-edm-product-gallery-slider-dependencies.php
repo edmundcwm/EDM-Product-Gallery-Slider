133fa91446b443fc33641d3be51ddc1f29cd26d9
Edmund Chan <edmundcwm@gail.com>
2020-06-22 23:13:53 +0800

    Update formatting

--- includes/class-edm-product-gallery-slider-dependencies.php
+++ includes/class-edm-product-gallery-slider-dependencies.php
@@ -7,23 +7,23 @@ defined( 'ABSPATH' ) || exit;
  */
 class EDM_Product_Gallery_Slider_Dependencies {
 
-    /**
-     * List of dependencies (required plugins)
-     */
-    const REQUIRED_PLUGINS = array(
-        'Woocommerce' => 'woocommerce/woocommerce.php',
-    );
+	/**
+	 * List of dependencies (required plugins)
+	 */
+	const REQUIRED_PLUGINS = array(
+		'Woocommerce' => 'woocommerce/woocommerce.php',
+	);
 
-    /**
-     * The list of active plugins
-     *
-     * @since  1.0.0
-     * @access private
-     * @var    array $active_plugins Stores the list of active plugins
-     */
-    private $active_plugins;
-    
-    /**
+	/**
+	 * The list of active plugins
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @var    array $active_plugins Stores the list of active plugins
+	 */
+	private $active_plugins;
+	
+	/**
 	 * The unique identifier of this plugin.
 	 *
 	 * @since    1.0.0
@@ -31,35 +31,30 @@ class EDM_Product_Gallery_Slider_Dependencies {
 	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
 	 */
 	protected $plugin_name;
+	
+	public function check() {
+		$missing_plugins = $this->get_missing_plugins_list();
+		if ( ! empty( $missing_plugins ) ) {
+			throw new EDM_Product_Gallery_Slider_Dependencies_Exception( $missing_plugins );
+		}
+	}
 
-    // public function __construct() {
-    //     // $this->plugin_name = $plugin_name;
-    //     $this->active_plugins = get_option( 'active_plugins' );
-    // }
-    
-    public function check() {
-        $missing_plugins = $this->get_missing_plugins_list();
-        if ( ! empty( $missing_plugins ) ) {
-            throw new EDM_Product_Gallery_Slider_Dependencies_Exception( $missing_plugins );
-        }
-    }
+	private function get_missing_plugins_list() {
+		$missing_plugins = array();
+		foreach ( self::REQUIRED_PLUGINS as $plugin => $plugin_path ) {
+			if ( ! $this->check_is_active( $plugin_path ) ) {
+				$missing_plugins[] = $plugin;
+			}
+		}
 
-    private function get_missing_plugins_list() {
-        $missing_plugins = array();
-        foreach ( self::REQUIRED_PLUGINS as $plugin => $plugin_path ) {
-            if ( ! $this->check_is_active( $plugin_path ) ) {
-                $missing_plugins[] = $plugin;
-            }
-        }
+		return $missing_plugins;
+	}
 
-        return $missing_plugins;
-    }
+	private function get_active_plugins() {
+		return get_option( 'active_plugins' );
+	}
 
-    private function get_active_plugins() {
-        return get_option( 'active_plugins' );
-    }
-
-    private function check_is_active( $plugin_path ) {
-        return in_array( $plugin_path, $this->get_active_plugins() );
-    }
+	private function check_is_active( $plugin_path ) {
+		return in_array( $plugin_path, $this->get_active_plugins() );
+	}
 }
--- public/class-edm-product-gallery-slider-public.php
+++ public/class-edm-product-gallery-slider-public.php
@@ -5,79 +5,79 @@
  */
 class EDM_Product_Gallery_Slider_Public {
 
-    /**
+	/**
 	 * The unique identifier of this plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
 	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
 	 */
-    private $plugin_name;
-    
-    /**
+	private $plugin_name;
+	
+	/**
 	 * The current version of the plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
 	 * @var      string    $version    The current version of the plugin.
 	 */
-    private $version;
+	private $version;
 
-    public function __construct( $plugin_name, $plugin_version ) {
-        $this->plugin_name = $plugin_name;
-        $this->version = $plugin_version;
-    }
+	public function __construct( $plugin_name, $plugin_version ) {
+		$this->plugin_name = $plugin_name;
+		$this->version = $plugin_version;
+	}
 
-    /**
-     * Enqueue public facing scripts
-     */
-    public function enqueue_scripts() {
-        if ( is_singular( 'product' ) ) {
-            wp_enqueue_script( 'frontend-js', plugin_dir_url( __FILE__ ) . 'js/frontend.js', array( 'jquery', 'flexslider' ), $this->version, true );
-        }
-    }
+	/**
+	 * Enqueue public facing scripts
+	 */
+	public function enqueue_scripts() {
+		if ( is_singular( 'product' ) ) {
+			wp_enqueue_script( 'frontend-js', plugin_dir_url( __FILE__ ) . 'js/frontend.js', array( 'jquery', 'flexslider' ), $this->version, true );
+		}
+	}
 
-    /**
-     * Enqueue public facing styles
-     */
-    public function enqueue_styles() {
-        if ( is_singular( 'product' ) ) {
-            wp_enqueue_style( 'frontend-css', plugin_dir_url( __FILE__ ) . 'css/frontend.css', array(), $this->version );
-            if ( 'yes' === get_option( 'edm_include_fa' ) ) { //only enqueue FA5 if admin option is checked
-                wp_enqueue_style( 'fa5', plugin_dir_url( __FILE__ ) . 'css/fontawesome.min.css', array(), '5.9.0' );
-                wp_enqueue_style( 'fa5-solid', plugin_dir_url( __FILE__ ) . 'css/solid.min.css', array(), '5.9.0' );
-            }
-        }
-    }
+	/**
+	 * Enqueue public facing styles
+	 */
+	public function enqueue_styles() {
+		if ( is_singular( 'product' ) ) {
+			wp_enqueue_style( 'frontend-css', plugin_dir_url( __FILE__ ) . 'css/frontend.css', array(), $this->version );
+			if ( 'yes' === get_option( 'edm_include_fa' ) ) { //only enqueue FA5 if admin option is checked
+				wp_enqueue_style( 'fa5', plugin_dir_url( __FILE__ ) . 'css/fontawesome.min.css', array(), '5.9.0' );
+				wp_enqueue_style( 'fa5-solid', plugin_dir_url( __FILE__ ) . 'css/solid.min.css', array(), '5.9.0' );
+			}
+		}
+	}
 
-    /**
-     * Include WC template override within plugin
-     */
-    public function template_override( $template, $template_name, $template_path ) {
-        $plugin_path = plugin_dir_path( __FILE__ ) . $template_path . $template_name;
+	/**
+	 * Include WC template override within plugin
+	 */
+	public function template_override( $template, $template_name, $template_path ) {
+		$plugin_path = plugin_dir_path( __FILE__ ) . $template_path . $template_name;
 
-        return file_exists( $plugin_path ) ? $plugin_path : $template;
-    }
+		return file_exists( $plugin_path ) ? $plugin_path : $template;
+	}
 
-    public function disable_photo_swipe() {
-        return false;
-    }
+	public function disable_photo_swipe() {
+		return false;
+	}
 
-    /**
-     * Modify Product Gallery FlexSlider options
-     * 
-     * @param  array $options FlexSlider options
-     * @return array $options Modified options
-     */
-    public function product_carousel_options( $options ) {
-        $options['controlNav'] = false;
-        $options['sync'] = '#carousel';
+	/**
+	 * Modify Product Gallery FlexSlider options
+	 * 
+	 * @param  array $options FlexSlider options
+	 * @return array $options Modified options
+	 */
+	public function product_carousel_options( $options ) {
+		$options['controlNav'] = false;
+		$options['sync'] = '#carousel';
 
-        return $options;
-    }
+		return $options;
+	}
 
-    public function load_template_functions() {
-        require_once plugin_dir_path( __FILE__ ) . 'edm-product-gallery-template-functions.php';
-    }
+	public function load_template_functions() {
+		require_once plugin_dir_path( __FILE__ ) . 'edm-product-gallery-template-functions.php';
+	}
 
 }
